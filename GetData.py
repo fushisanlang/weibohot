1203b3ea0b98ab6818182d9e1524d2ce4aade13f
fushisanlang <313346216@qq.com>
2022-03-18 08:48:39 +0800

    优化selenium

--- GetData.py
+++ GetData.py
@@ -24,6 +24,11 @@ def GetData():
     # 定义无窗口运行,否则在服务器环境会报错
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("blink-settings=imagesEnabled=false")
+    chrome_options.add_argument("--disable-extensions")
+
     chrome_options.add_argument('--headless')
     browser = webdriver.Chrome(
         chrome_options=chrome_options, executable_path="/usr/local/bin/chromedriver")
